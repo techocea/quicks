a0caaad1befa5295601407fcfd2a075d5f225a5c
techocea <dunsfordbright@gmail.com>
2024-04-10 20:49:52 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#010314",
+        green: "#5AFE73",
+      },
+    },
+  },
+  plugins: [],
+};
